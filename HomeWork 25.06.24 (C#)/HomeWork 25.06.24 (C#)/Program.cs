a91087d9de25a0ea18772217affe2d89e563042c
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-06-23 11:34:10 +0200

    Add files via upload

---
+++ HomeWork 25.06.24 (C#)/HomeWork 25.06.24 (C#)/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeWork_25._06._24__C__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //я надеюсь так считается)) я пытался максимально коротко сделать
+            string text1 = "Alex"; 
+            Console.WriteLine(text1);
+            char[] charArray1 = text1.ToCharArray();
+            HashSet<string> charSet1 = new HashSet<string>();
+
+            for (int i = 0; i < charArray1.Length; i++)
+            {
+                Random.Shared.Shuffle(charArray1);
+                charSet1.Add(new string(charArray1));
+            }
+
+            foreach(var c in charSet1)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
